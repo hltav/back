8a11edf2e444479bfd15b87333e2a2373496e4c0
Hudson Lima Tavares <hudsonlimatavares@gmail.com>
2023-04-05 18:36:46 -0300

    relacionamento de tabelas

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,12 +1,13 @@
 import * as Yup from 'yup';
 import Product from '../models/Product';
+import Category from '../models/Category';
 
 class ProductController {
   async store(request, response) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       price: Yup.number().required(),
-      category: Yup.string().required(),
+      category_id: Yup.number().required(),
     });
 
     try {
@@ -15,12 +16,12 @@ class ProductController {
       return response.status(400).json({ error: err.errors });
     }
     const { filename: path } = request.file;
-    const { name, price, category } = request.body;
+    const { name, price, category_id } = request.body;
 
     const product = await Product.create({
       name,
       price,
-      category,
+      category_id,
       path,
     });
 
@@ -28,7 +29,15 @@ class ProductController {
   }
 
   async index(request, response) {
-    const products = await Product.findAll();
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          as: 'category',
+          attributes: ['id', 'name'],
+        },
+      ],
+    });
 
     return response.json(products);
   }
